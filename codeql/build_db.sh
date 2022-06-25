7afd4de43167b82874c6d724132eb66ba5dd7486
Jens Dietrich <jens.dietrich@gmail.com>
2022-06-25 16:18:40 +1200

    improvement (mvn compile is sufficient to build the codeql db)

--- codeql/build_db.sh
+++ codeql/build_db.sh
@@ -7,8 +7,4 @@ then
     exit 1
 fi
 # clean is necessary if classes already exist, codeql will complain that no classes have been found
-codeql database create $DB -l java -c 'mvn clean install'
-
-
-
-
+codeql database create $DB -l java -c 'mvn clean compile'
