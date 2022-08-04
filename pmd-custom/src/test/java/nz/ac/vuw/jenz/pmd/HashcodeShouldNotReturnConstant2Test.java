10fb1860ee6cc04a98b079bbfd65602e0db56f3c
Jens Dietrich <jens.dietrich@gmail.com>
2022-08-04 12:58:18 +1200

    added some notes to help me remember how this works when I lecture

--- pmd-custom/src/test/java/nz/ac/vuw/jenz/pmd/HashcodeShouldNotReturnConstant2Test.java
+++ pmd-custom/src/test/java/nz/ac/vuw/jenz/pmd/HashcodeShouldNotReturnConstant2Test.java
@@ -2,5 +2,6 @@ package nz.ac.vuw.jenz.pmd;
 
 import net.sourceforge.pmd.testframework.PmdRuleTst;
 
+// see XML files in resources for actual tests
 public class HashcodeShouldNotReturnConstant2Test extends PmdRuleTst {
 }
